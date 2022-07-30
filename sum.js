bc85c6384824e5b1842f25d5a844ea31dd0af1a0
Wen <owilalala974@gmail.com>
2022-07-30 17:07:08 +0800

    mod hello.js & sum.js

--- sum.js
+++ sum.js
@@ -1,5 +1,6 @@
 //sum.js
 
+//方法一
 function sum(n) {
     //1+2+3+....n
     // return n;
@@ -8,9 +9,15 @@ function sum(n) {
         result = result + i;
     }
     return result;
-
 }
 
 console.log(sum(1));//1
 console.log(sum(3));//6
 console.log(sum(10));//55
+
+//方法二
+function sum2(n) {
+    return (n + 1) * n / 2;
+}
+
+console.log(sum2(10));
