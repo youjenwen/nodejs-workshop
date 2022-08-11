b6fd8a50f2bc5ed1c049984ba306e19d3844dc16
Wen <owilalala974@gmail.com>
2022-08-11 10:34:51 +0800

    new readfile-optional

---
+++ promise/readfile-optional.js
@@ -0,0 +1,34 @@
+// import { readFile } from 'fs/promises';
+const fs = require('fs')
+
+const controller = new AbortController();
+const { signal } = controller;
+let modReadfile = function (file) {
+    return new Promise((resolve, rejects) => {
+
+        // console.log(controller)
+        // console.log(signal.aborted)
+        // controller.abort();
+        fs.readFile(file, (err, data) => {
+            //如果中止或是檔案錯誤
+            if (signal.aborted || err) {
+                rejects(err)
+                // console.log(`modReadFile err: ${err}`);
+            }
+            resolve(data)
+        });
+
+    })
+}
+async function test(file) {
+    try {
+        // Abort the request before the promise settles.
+        // controller.abort();//中止
+        let result = await modReadfile(file);
+        console.log(`result: ${result}`);
+    } catch (err) {
+        // When a request is aborted - err is an AbortError
+        console.error("AbortError!!");
+    }
+}
+test('test.txt')
