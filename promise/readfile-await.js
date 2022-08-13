cc6c11044adc11c848d11250a5feb2e623e316ae
Wen <owilalala974@gmail.com>
2022-08-13 11:04:48 +0800

    mod readfile-await 老師版本

--- promise/readfile-await.js
+++ promise/readfile-await.js
@@ -1,22 +1,50 @@
 const fs = require('fs');
 
-let promiseFile = function (fileName) {
-    return new Promise((resolve, rejects) => {
-        fs.readFile(fileName, 'utf8', (err, data) => {
-            if (err) {
-                rejects(err);
-            }
-            resolve(data);
-        });
-    })
-}
-async function asyncFile(file){
-    try{
-        let result = await promiseFile(file)
-        console.log(result);
+// let promiseFile = new Promise((resolve, rejects) => {
+//     fs.readFile('test.txt', 'utf8', (err, data) => {
+//         if (err) {
+//             rejects(err);
+//         }
+//         resolve(data);
+//     });
+// })
+
+// (async () => {
+//         try {
+//             let result = await promiseFile
+//             console.log(result);
+//         }
+//         catch (err) {
+//             console.log(`發生錯誤${err}`);
+//         }
+//     })();
+let p = new Promise((resolve, reject) => {
+    fs.readFile('test.txt', 'utf8', (err, data) => {
+      if (err) {
+        return reject(err); // if(status === pending) promise status -> rejected
+      }
+      // 如果 reject 沒有加上 return
+      // 這行會被印出來 (即使是 reject 時也是)
+      resolve(data); // if(status === pending) promise status -> fulfilled
+    });
+  });
+
+
+(async () => {
+    try {
+      let data = await p;
+      console.log('await', data);
+    } catch (e) {
+      console.error(e);
     }
-    catch(err){
-        console.log(`發生錯誤${err}`);
-    }
-}
-asyncFile('test.txt');
+  })();
+// async function asyncFile() {
+//     try {
+//         let result = await promiseFile
+//         console.log(result);
+//     }
+//     catch (err) {
+//         console.log(`發生錯誤${err}`);
+//     }
+// }
+// asyncFile();
