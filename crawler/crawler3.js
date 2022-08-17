535c6f7b8d403de0a022a93981017a8e46767e0d
wen <owilalala974@gmail.com>
2022-08-17 22:00:57 +0800

    mod setting Eslint

--- crawler/crawler3.js
+++ crawler/crawler3.js
@@ -3,44 +3,43 @@ const moment = require('moment');
 const fs = require('fs');
 
 let stockFile = new Promise((resolve, rejects) => {
-    fs.readFile('stockNo.txt', 'utf8', (err, data) => {
-        if (err) {
-            rejects(err);
-        }
-        resolve(data);
-    });
+  fs.readFile('stockNo.txt', 'utf8', (err, data) => {
+    if (err) {
+      rejects(err);
+    }
+    resolve(data);
+  });
 });
 
 (async () => {
-    try {
-        let data = await stockFile;
+  try {
+    let data = await stockFile;
 
-        let toDay = moment().format('YYYYMMDD');
-        let stockNo = data;
-        (async () => {
-            try {
-                let response = await axios.get('https://www.twse.com.tw/exchangeReport/STOCK_DAY?', {
-                    params: {
-                        response: 'json',
-                        date: toDay,
-                        stockNo: stockNo,
-                    }
-                });
-                console.log(response);
-            } catch (e) {
-                console.error(e);
-            }
-        })();
-    } catch (e) {
-        console.error(e)
-    }
+    let toDay = moment().format('YYYYMMDD');
+    let stockNo = data;
+    (async () => {
+      try {
+        let response = await axios.get(
+          'https://www.twse.com.tw/exchangeReport/STOCK_DAY?',
+          {
+            params: {
+              response: 'json',
+              date: toDay,
+              stockNo: stockNo,
+            },
+          }
+        );
+        console.log(response.data);
+      } catch (e) {
+        console.error(e);
+      }
+    })();
+  } catch (e) {
+    console.error(e);
+  }
 })();
 
-
 // console.log(moment().format('YYYYMMDD'))
-
-
-
 
 // axios
 //     .get('https://www.twse.com.tw/exchangeReport/STOCK_DAY?response=json&date=20220813&stockNo=2330&_=1660378514253')
