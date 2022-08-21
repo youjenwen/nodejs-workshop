5e20d9903bd368600eaf63c16f85c5849ab1dbb1
Wen <owilalala974@gmail.com>
2022-08-21 17:10:42 +0800

    mod 新增API

--- stock-be/server.js
+++ stock-be/server.js
@@ -5,9 +5,46 @@ require('dotenv').config();
 const app = express();
 //從.env讀取
 const port = process.env.SERVER_PORT;
+const cors = require('cors');
+app.use(cors());
 
+const mysql = require('mysql2');
+let pool = mysql
+  .createPool({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  })
+  .promise();
+
+//API
+app.get('/api/1.0/stocks', async (req, res, next) => {
+  // console.log(process.env.DB_NAME);
+  // let result = await pool.execute('SELECT * FROM stocks');
+  // let data = result[0];
+
+  let [data] = await pool.execute('SELECT * FROM stocks');
+
+  // console.log(data);
+  res.json(data);
+});
+
+app.use((req, res, next) => {
+  console.log('這是第1個');
+  next();
+});
+// app.[method]
+// method: get, post, delete, put, patch, ...
+// GET /
 app.get('/', (req, res) => {
-  res.end('Hello Express');
+  res.send('這裡是首頁');
+});
+
+app.use((req, res, next) => {
+  console.log('404');
+  res.status(404).send('Not Found!');
 });
 
 app.listen(port, () => {
