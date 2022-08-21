9a9be14a90567943ced948bcf32d24f8979746b9
Wen <owilalala974@gmail.com>
2022-08-21 09:11:19 +0800

    school

--- crawler/crawler5.js
+++ crawler/crawler5.js
@@ -2,31 +2,40 @@ const axios = require('axios');
 const moment = require('moment');
 const fs = require('fs/promises');
 const mysql = require('mysql2');
-
+require('dotenv').config();
 
 (async () => {
+    // console.log('DB_HOST', process.env.DB_HOST);
     let connection;
+
     try {
         connection = mysql.createConnection({
-            host: 'localhost',
-            port: 3306,
-            user: '',
-            password: '',
-            database: 'stock_mfee27',
+            host: process.env.DB_HOST,
+            port: process.env.DB_PORT,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME,
         });
+        // connection = mysql.createConnection({
+        //     host: 'localhost',
+        //     port: 3306,
+        //     user: '',
+        //     password: '',
+        //     database: 'stock_mfee27',
+        // });
         // let data = await fs.readFile('stockNo.txt', 'utf8');
         // console.log(data);
         // let stockFile = fs.readFile('stockNo.txt', 'utf8')
         // let toDay = moment().format('YYYY/MM/DD');
         let stockNo = await fs.readFile('stockNo.txt', 'utf8');
         // console.log(stockNo);
-        let response = await axios.get('https://www.twse.com.tw/zh/api/codeQuery', {
+        let queryNameResponse = await axios.get('https://www.twse.com.tw/zh/api/codeQuery', {
             params: {
                 query: stockNo
-            }
+            },
         });
-        console.log(response.data);
-        let suggestions = response.data.suggestions;
+        console.log(queryNameResponse.data);
+        let suggestions = queryNameResponse.data.suggestions;
         // console.log(suggestions);
         let suggestion = suggestions[0]
         if (suggestion === "(無符合之代碼或名稱)") {
@@ -34,11 +43,18 @@ const mysql = require('mysql2');
             throw new Error(suggestion);//丟錯誤
         }
         let stockName = suggestion.split('\t').pop();
-        console.log(stockName);
+        // console.log(stockName);
 
         let saveNameResult = connection.execute(`INSERT IGNORE INTO stocks (id, name) VALUES (?, ?)`, [stockNo, stockName]);
-        console.log(saveNameResult);
-
+        // console.log(saveNameResult);
+        let queryDate = moment().format('YYYYMMDD');
+        let response = await axios.get(`https://www.twse.com.tw/exchangeReport/STOCK_DAY`,{
+            params:{
+                response: 'json',
+                date: queryDate,
+                stockNo: stockNo,
+            },
+        });
     } catch (e) {
         console.error(e);
     } finally {
