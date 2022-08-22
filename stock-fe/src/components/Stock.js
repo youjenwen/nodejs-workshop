2ebd8688bd63c4096647c37791cf884bddadca57
Wen <owilalala974@gmail.com>
2022-08-22 09:01:03 +0800

    new stock-fe

---
+++ stock-fe/src/components/Stock.js
@@ -0,0 +1,39 @@
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+const Stock = () => {
+  const [error, setError] = useState(null);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    let getStock = async () => {
+      let response = await axios.get('http://localhost:3001/api/1.0/stocks');
+      console.log(response.data);
+      setData(response.data);
+    };
+    getStock();
+  });
+
+  return (
+    <div>
+      {error && <div>{error}</div>}
+      <h2 className="ml-7 mt-6 text-xl text-gray-600">股票代碼</h2>
+      {/* response 的資料利用map拆開 */}
+      {data.map((stock) => {
+        return (
+          <div className="bg-white bg-gray-50 p-6 rounded-lg shadow hover:shadow-lg m-6 cursor-pointer">
+            <Link to={`/stock/1234`}>
+              <h2 className="text-2xl font-bold mb-2 text-gray-800">
+                {stock.id}
+              </h2>
+              <p className="text-gray-700">{stock.name}</p>
+            </Link>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Stock;
