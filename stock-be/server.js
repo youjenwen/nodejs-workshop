8238d9317d46f9186fcefff4b3f48f2458acd0bd
Wen <owilalala974@gmail.com>
2022-08-28 13:44:35 +0800

    mod stock-be & stock-fe 新增頁數串接

--- stock-be/server.js
+++ stock-be/server.js
@@ -30,30 +30,47 @@ app.get('/api/1.0/stocks', async (req, res, next) => {
   // console.log(process.env.DB_NAME);
   // let result = await pool.execute('SELECT * FROM stocks');
   // let data = result[0];
-
   let [data] = await pool.execute('SELECT * FROM stocks');
-
   // console.log(data);
   res.json(data);
 });
 //stockDetails API所有股票代碼
 app.get('/api/1.0/stocks/:stockId', async (req, res, next) => {
-  const { stockId } = req.params;
-  // console.log(process.env.DB_NAME);
-
-  //要怎麼得到id? 利用params
-  //直接貼sql語法會有問題 -> sql injection 如果在網址裡輸入 || 1=1 -- 全部資料都會顯示
-  // let [data] = await pool.execute(
-  //   `SELECT * FROM stock_prices WHERE stock_id=${stockId}`
-  // );
-  //
-  let [data] = await pool.execute(
-    'SELECT * FROM stock_prices WHERE stock_id=?',
+  const stockId = req.params.stockId;
+  //一頁幾筆
+  let perPage = 3;
+  //第幾頁 GET /api/1.0/stock/:stockId?page=1
+  //預設1
+  let page = req.query.page || 1;
+  //顯示第1頁要跳過前0筆資料，從第1筆開始
+  //0~3 page=1
+  //顯示第2頁要跳過前3筆資料，從第4筆開始
+  //4~6 page=2......
+  //offset 欲跳過筆數
+  let offset = perPage * (page - 1);
+  //取得總筆數
+  let [total] = await pool.execute(
+    'SELECT COUNT(*) AS total FROM stock_prices WHERE stock_id=?',
     [stockId]
   );
-
-  console.log(data);
-  res.json(data);
+  // console.log('total', total[0].total);
+  total = total[0].total;
+  let lastPage = Math.ceil(total / perPage);
+  // console.log('lastpage', lastPage);
+  let [data] = await pool.execute(
+    'SELECT * FROM stock_prices WHERE stock_id = ? ORDER BY date LIMIT ? OFFSET ?',
+    [stockId, perPage, offset]
+  );
+  // console.log(data);
+  res.json({
+    pagination: {
+      total, //總頁數
+      perPage, //一頁幾筆
+      page, //目前第幾頁
+      lastPage, //總共幾頁
+    },
+    data,
+  });
 });
 
 app.use((req, res, next) => {
--- stock-fe/src/components/StockDetails.js
+++ stock-fe/src/components/StockDetails.js
@@ -2,42 +2,72 @@ import axios from 'axios';
 import React from 'react';
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import moment from 'moment';
+
+//import moment from 'moment';
 
 const StockDetails = () => {
   const { stockId } = useParams(); //不能是陣列接
   // console.log(stockId);
   const [error, setError] = useState(null);
   const [data, setData] = useState([]);
+  const [lastPage, setLastPage] = useState(1);
+  const [page, setPage] = useState(1);
 
   useEffect(() => {
     let getStock = async () => {
       let response = await axios.get(
-        `http://localhost:3001/api/1.0/stocks/${stockId}`
+        `http://localhost:3001/api/1.0/stocks/${stockId}?page=${page}`
       );
-      setData(response.data);
+      setData(response.data.data);
       // console.log('useEffect[] after set', response.data);
+      setLastPage(response.data.pagination.lastPage);
+      // console.log('response.data.pagination.lastPage',response.data.pagination.lastPage)
     };
     getStock();
-  }, []);
+  }, [page]);
+
+  let getPages = () => {
+    let pages = [];
+    for (let i = 1; i <= lastPage; i++) {
+      // console.log(i)
+      pages.push(
+        <li
+          style={{
+            display: 'inline-block',
+            margin: '2px',
+            backgroundColor: page === i ? '#00d1b2' : '',
+            borderColor: page === i ? '#00d1b2' : '#dbdbdb',
+            color: page === i ? '#fff' : '#363636',
+            borderWidth: '1px',
+            width: '28px',
+            height: '28px',
+            borderRadius: '3px',
+            textAlign: 'center',
+          }}
+          key={i}
+          onClick={(e) => {
+            setPage(i);
+          }}
+        >
+          {i}
+        </li>
+      );
+    }
+    return pages;
+  };
 
   return (
     <div>
       {error && <div>{error}</div>}
+      <ul>{getPages()}</ul>
       {data.map((stockDetails) => {
-        //2022-07-31T16:00:00.000Z
-        //console.log(stockDetails.date);
-        //日期需要轉換  ISO-8601
-        const date = moment(new Date(stockDetails.date)).format('YYYY-MM-DD');
-        //console.log('date', date);
-
         return (
           <div
             key={Math.random().toString(36).replace('0.', '')}
             className="bg-white bg-gray-50 p-6 rounded-lg shadow m-6"
           >
             <h2 className="text-2xl font-bold mb-2 text-gray-800">
-              日期：{date}
+              日期：{stockDetails.date}
             </h2>
             <h2 className="text-2xl font-bold mb-2 text-gray-800">
               成交金額：{stockDetails.amount}
