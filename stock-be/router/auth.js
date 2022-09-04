98c29e388595d5f3e1782f9c5025fd5741280fd8
wen <owilalala974@gmail.com>
2022-09-04 08:04:25 +0800

    mod 新增註冊時驗證

--- stock-be/router/auth.js
+++ stock-be/router/auth.js
@@ -2,10 +2,36 @@ const { response } = require('express');
 const express = require('express');
 const router = express.Router();
 const pool = require('../utils/db'); //引入資料庫
+//安裝 引用bcrypt
 const bcrypt = require('bcrypt');
 
-router.post('/api/1.0/auth/register', async (req, res, next) => {
+//安裝 引用express-validator
+const { body, validationResult } = require('express-validator');
+
+const registerRules = [
+  //中間件：檢查email
+  body('email').isEmail().withMessage('Email欄位請填寫正確格式'),
+  //中間件：檢查password
+  body('password').isLength({ min: 8 }).withMessage('密碼長度至少為8'),
+  //中間件： 檢查password & confirmPassword 是否一致(客製)
+  body('confirmPassword')
+    .custom((value, { req }) => {
+      return value === req.body.password;
+    })
+    .withMessage('密碼驗證不一致'),
+];
+
+router.post('/api/1.0/auth/register', registerRules, async (req, res, next) => {
   console.log('register', req.body);
+  //驗證前端資料
+  const validateResult = validationResult(req);
+  console.log('validateResult ', validateResult);
+
+  if (!validateResult.isEmpty()) {
+    //validateResult 不為空 則有錯誤需回傳給前端
+    return res.status(400).json({ errors: validateResult.array() });
+  }
+
   let [member] = await pool.execute('SELECT * FROM members WHERE email = ?', [
     req.body.email,
   ]);
